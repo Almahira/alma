0b7a2b6a6d21fa1c71c3da70e0abc4ede06a4783
Rendi Faizal <botak1232q@gmail.com>
2026-09-16 16:15:35 +0700

    feat: enhanced express

--- apps/server_unv/src/index.ts
+++ apps/server_unv/src/index.ts
@@ -175,7 +175,8 @@ const io = new Server(httpServer, {
 });
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use("/api/payment", paymentRouter);
 app.use("/api/storage", storageRouter);
 app.use("/api/provision", provisionRouter);
